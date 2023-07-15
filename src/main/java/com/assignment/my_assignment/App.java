0ee1f04ecaf52cf6cf60ec771b9ca055773c2fc0
Saaketh <saaketh.g1308@gmail.com>
2023-07-14 22:09:34 -0400

    new commit

---
+++ src/main/java/com/assignment/my_assignment/App.java
@@ -0,0 +1,40 @@
+package com.assignment.my_assignment;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//comment the above line and uncomment below line to use Chrome
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        //System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver","/Users/Saaketh/Documents/Software/driver/chromedriver.exe");
+        
+       ChromeOptions chromeoptions = new ChromeOptions();
+       WebDriver driver = new ChromeDriver(chromeoptions);
+       driver.get("http://18.223.248.64:8080/addressbook-demo/");
+       Thread.sleep(10000);
+       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+       
+       driver.findElement(By.className("v-button")).click();
+       
+       driver.findElement(By.id("gwt-uid-5")).sendKeys("Saaketh");
+       driver.findElement(By.id("gwt-uid-7")).sendKeys("G");
+       driver.findElement(By.id("gwt-uid-9")).sendKeys("7940012259");
+       driver.findElement(By.id("gwt-uid-11")).sendKeys("Saaketh@gmail.com");
+       driver.findElement(By.id("gwt-uid-13")).sendKeys("4/3/21,");
+       
+       driver.findElement(By.className("v-button-primary")).click();
+       Thread.sleep(10000);
+       driver.close();
+       driver.quit();
+    }
+}
---
+++ src/test/java/com/assignment/my_assignment/DummyTests.java
@@ -0,0 +1,30 @@
+package com.assignment.my_assignment;
+
+import org.junit.Test;
+
+
+public class DummyTests {
+	
+	 @Test
+	    public void thisisAssignmentTest1()
+	    {
+	        System.out.println("This is a First Dummy Test for Assignment");
+	    }
+	 
+	 @Test
+	    public void thisisAssignmentTest2()
+	    {
+	        System.out.println("This is a Second Dummy Test for Assignment");
+	    }
+	 
+	 @Test
+	    public void thisisAssignmentTest3()
+	    {
+	        System.out.println("This is a Third Dummy Test for Assignment");
+	    }
+
+}
+
+
+
+
