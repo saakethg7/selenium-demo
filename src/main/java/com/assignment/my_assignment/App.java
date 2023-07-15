a2db5ba16b393a0ebde6424676ab19be84431c91
Saaketh <saaketh.g1308@gmail.com>
2023-07-14 22:52:00 -0400

    modified path

--- src/main/java/com/assignment/my_assignment/App.java
+++ src/main/java/com/assignment/my_assignment/App.java
@@ -16,7 +16,7 @@ public class App
     public static void main( String[] args ) throws InterruptedException
     {
         //System.out.println( "Hello World!" );
-        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
         
        ChromeOptions chromeoptions = new ChromeOptions();
        WebDriver driver = new ChromeDriver(chromeoptions);
